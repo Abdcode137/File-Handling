0593e667a0fff4adc8c897973775bd7239af9212
Abdcode137 <abdulrehman.qamer123@gmail.com>
2025-04-10 17:39:46 +0500

    Add files via upload

---
+++ A.Cprogram.py
@@ -0,0 +1,13 @@
+f = open("Codingal.txt","r")
+
+f.read()
+f.close()
+
+f = open("Codingal.txt","w")
+
+f.write('Hi! I am a penguin and I am 1 year old. ')
+f.close()
+
+f = open("Codingal.txt","a")
+f.write("Hi ! Iam your friend.")
+f.close()
